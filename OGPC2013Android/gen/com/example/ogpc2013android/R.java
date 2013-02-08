35a8d290d913180ed0ad254c58be9582746e31ac
william <wpapsco@gmail.com>
2013-02-07 23:16:55 -0800

    PRESS BUTTON, OBTAIN LEVEL SELECT (kinda)

--- OGPC2013Android/gen/com/example/ogpc2013android/R.java
+++ OGPC2013Android/gen/com/example/ogpc2013android/R.java
@@ -10,22 +10,37 @@ package com.example.ogpc2013android;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int transparent=0x7f040001;
+        public static final int white=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int level_animation=0x7f020001;
+        public static final int location0=0x7f020002;
+        public static final int location1=0x7f020003;
+        public static final int location2=0x7f020004;
+        public static final int start=0x7f020005;
+        public static final int title_background=0x7f020006;
+        public static final int tutorial_level_select=0x7f020007;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int imageButton1=0x7f080002;
+        public static final int imageView1=0x7f080000;
+        public static final int imageView2=0x7f080001;
+        public static final int menu_settings=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_level_select=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -43,10 +58,10 @@ public final class R {
     
  API 11 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- OGPC2013Android/src/com/example/ogpc2013android/MainActivity.java
+++ OGPC2013Android/src/com/example/ogpc2013android/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.ogpc2013android;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +24,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void onImageButtonClicked(View view) {
+		Intent intent = new Intent(this, LevelSelectActivity.class);
+		startActivity(intent);
 	}
 
 }
